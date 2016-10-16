d7fa3031e89925458278bc3d3bbb5e7e590f04a5
edsong1257 <admin98@gmail.com>
2016-10-16 16:12:14 +0800

    test_v1

--- spec/twitter_api_spec.rb
+++ spec/twitter_api_spec.rb
@@ -4,12 +4,13 @@ require 'yaml'
 require './lib/twitter_lib.rb'
 require_relative '../script'
 CREDENTIALS = YAML.load(File.read('config/credentials.yml'))
+tag_text = 'food'
 tags = "#food"
 
 
 describe 'Be able to authorized by twitter' do
   before do
-  Twitter::Client.new(api_key: CREDENTIALS["api_key"], api_secret: CREDENTIALS["api_secret"])
+    Twitter::Client.new(api_key: CREDENTIALS["api_key"], api_secret: CREDENTIALS["api_secret"])
   end
   it 'HAPPY: should be authenticated by twitter' do
   last_response = HTTParty.get('https://api.twitter.com/1.1/search/tweets.json',
@@ -17,16 +18,30 @@ describe 'Be able to authorized by twitter' do
                  query: { q: tags})
     _(last_response.code).must_equal 200
   end
+
+  it 'SAD: should NOT be authenticated by twitter' do
+  last_response = HTTParty.get('https://api.twitter.com/1.1/search/tweets.json',
+                 query: { q: tags })
+    _(last_response.code).must_equal 400
+  end
 end
 
 describe 'Be able to get tweets with specific Hashtags' do
   before do
   Twitter::Client.new(api_key: CREDENTIALS["api_key"], api_secret: CREDENTIALS["api_secret"])
   end
-  it 'HAPPY: should be authenticated by twitter' do
+  it 'HAPPY: should be able to find tweets' do
   last_response = HTTParty.get('https://api.twitter.com/1.1/search/tweets.json',
                  headers: default_headers,
                  query: { q: tags})
     _(last_response.count).must_be :>, 0
+  end
+
+  it 'HAPPY: should be able to find tweets WITH specific hashtags' do
+    last_response = HTTParty.get('https://api.twitter.com/1.1/search/tweets.json',
+                 headers: default_headers,
+                 query: { q: tags})
+    result = last_response['statuses'][0]['entities']['hashtags'].map{|item| item['text']}
+    _(result).must_include tag_text
   end
 end

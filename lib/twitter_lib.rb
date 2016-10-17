555fafa8c5ea33e6ea0f856c7d444eb39285d7d2
Cheng-Yu Hsu <m@cyhsu.me>
2016-10-17 08:44:32 +0800

    Refine Twitter client

--- lib/twitter_lib.rb
+++ lib/twitter_lib.rb
@@ -3,35 +3,35 @@ require 'yaml'
 require 'base64'
 require 'httparty'
 
-moduel Twitter
-
+module Twitter
   class Client
-  
-  def initialize(api_key:, api_secret:)
-    @oauth_token ||= Base64.urlsafe_encode64("#{api_key}:#{api_secret}")
-    @default_header = default_headers
+    def initialize(api_key:, api_secret:)
+      @api_key = api_key
+      @api_secret = api_secret
+    end
+
+    def search_tweets(*tags)
+      HTTParty.get('https://api.twitter.com/1.1/search/tweets.json',
+                   headers: authorization_header,
+                   query: { q: tags.join(' ') }).parsed_response['statuses']
+    end
+
+    private
+
+    def oauth_token
+      @oauth_token ||= Base64.urlsafe_encode64("#{@api_key}:#{@api_secret}")
+    end
+
+    def authorization_response
+      @authorization_response ||= HTTParty.post('https://api.twitter.com/oauth2/token',
+                                                headers: { Authorization: "Basic #{oauth_token}" },
+                                                query: { grant_type: 'client_credentials' }).parsed_response
+    end
+
+    def authorization_header
+      @authorization_header ||= {
+        Authorization: "#{authorization_response['token_type'].capitalize} #{authorization_response['access_token']}"
+      }
+    end
   end
-
-  def search_tweets(*tags)
-    HTTParty.get('https://api.twitter.com/1.1/search/tweets.json',
-                 headers: default_header,
-                 query: { q: tags.join(' ') }).parsed_response['statuses']
-  end
-    
-  def authorize!
-    HTTParty.post('https://api.twitter.com/oauth2/token',
-                  headers: { Authorization: "Basic #{oauth_token}" },
-                  query: { grant_type: 'client_credentials' }).parsed_response
-  end
-
-  def default_headers
-    authorization_response = authorize!
-    @deafult_headers ||= {
-      Authorization: "#{authorization_response['token_type'].upcase} #{authorization_response['access_token']}"
-    }
-  end
-
-
-
-  
 end

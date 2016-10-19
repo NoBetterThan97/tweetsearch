d561d5c999bfe9d16c9a5816ad9176b55ec16f63
Cheng-Yu Hsu <m@cyhsu.me>
2016-10-20 07:59:27 +0800

    Split requirements to spec_helper

--- spec/twitter/client_spec.rb
+++ spec/twitter/client_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
-require 'minitest/autorun'
-require 'minitest/rg'
-require 'yaml'
+require './spec/spec_helper.rb'
 require './lib/twitter/client.rb'
 
 CREDENTIALS = YAML.load(File.read('config/credentials.yml'))
@@ -13,20 +11,3 @@ describe 'Twitter::Client#search_tweets' do
     client.search_tweets(tags).wont_be_nil
   end
 end
-
-# describe 'Be able to get tweets with specific Hashtags' do
-#   before do
-#     Twitter::Client.new(api_key: CREDENTIALS['api_key'], api_secret: CREDENTIALS['api_secret'])
-#   end
-#
-#   it 'HAPPY: should be able to find tweets' do
-#     last_response = search_tweets(tags)
-#     _(last_response.count).must_be :>, 0
-#   end
-#
-#   it 'HAPPY: should be able to find tweets WITH specific hashtags' do
-#     last_response = search_tweets(tags)
-#     result = last_response[0]['entities']['hashtags'].map { |item| item['text'].downcase }
-#     _(result).must_include tag_text
-#   end
-# end

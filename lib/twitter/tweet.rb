0d240419a3bfc39c01b99a8821dacd152bdf9dfc
Cheng-Yu Hsu <m@cyhsu.me>
2016-10-20 08:02:34 +0800

    Remove redundant STDOUT

--- lib/twitter/tweet.rb
+++ lib/twitter/tweet.rb
@@ -4,7 +4,6 @@ module Twitter
     attr_reader :id, :created_at, :text
 
     def initialize(data)
-      p data
       @id = data['id']
       @created_at = DateTime.parse(data['created_at'])
       @text = data['text']
@@ -12,7 +11,7 @@ module Twitter
 
     def self.search(*tags, using_client:)
       client = using_client
-      client.search_tweets(tags).map { |data| Tweet.new(data) }
+      client.search_tweets(tags).map { |data| new(data) }
     end
   end
 end

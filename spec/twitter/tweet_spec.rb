e3e71d9fe27a27734ede06f856b7519098fd64a5
Leo.li <leo.li@iss.nthu.edu.tw>
2016-10-23 17:31:14 +0800

    re-write tweets_spec

--- spec/twitter/tweet_spec.rb
+++ spec/twitter/tweet_spec.rb
@@ -2,6 +2,18 @@
 require_relative '../spec_helper.rb'
 
 describe 'Twitter::Tweet::search' do
+VCR.configure do |c|
+    c.cassette_library_dir = CASSETTES_FOLDER
+    c.hook_into :webmock
+
+    c.filter_sensitive_data('<ACCESS_TOKEN>') { CREDENTIALS[:access_token] }
+    c.filter_sensitive_data('<ACCESS_TOKEN_ESCAPED>') do
+      URI.parse(URI.encode(CREDENTIALS[:access_token].to_s))
+      #URI.escape(CREDENTIALS[:access_token])
+    end
+  end
+
+
   before do
     VCR.insert_cassette(cassette_name(__FILE__, name), record: :new_episodes)
 
